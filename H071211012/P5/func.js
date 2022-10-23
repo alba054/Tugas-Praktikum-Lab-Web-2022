e47a38a101d0b5dd60b0f14b405ad90534e8dea2
Mukramm_ <mukrammuhammad@gmail.com>
2022-10-23 15:12:59 +0800

    Membuat kartu As bisa menyesuaikan dan perbaiki beberapa bug

--- H071211012/P5/func.js
+++ H071211012/P5/func.js
@@ -2,6 +2,9 @@ let card = []; //Array kartu kosong yang nantinya akan diisi
 let cNum = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"]; //Nomor dan Simbol Kartu
 let cType = ["K", "L", "C", "S"]; //Tipe Kartu [K = Kelor (Clover/Keriting), L = Love (Hati), C = Ciduk (Diamond), S = Skop (Sekop)]
 
+let yourAce = 0;
+let dealerAce = 0;
+
 let canPlay;
 
 document.getElementById("resmo-button").disabled = true;
@@ -9,21 +12,6 @@ document.getElementById("hit-bt").disabled = true;
 document.getElementById("stand-bt").disabled = true;
 
 let yourResult, dealerResult = 0;
-
-// Membangun kartu
-for (let i = 0; i < cType.length; i++) {
-    for (let j = 0; j < cNum.length; j++) {
-        card.push(cNum[j] + "-" + cType[i]);
-    }
-}
-
-// Mengacak kartu
-for (let i = 0; i < card.length; i++) {
-    let j = Math.floor(Math.random() * card.length);
-    let shuf = card[i];
-    card[i] = card[j];
-    card[j] = shuf;
-}
 
 // Fungsi untuk tombol "Reset"
 function reset() {
@@ -50,12 +38,27 @@ function resmoButton() {
 
 // Fungsi untuk tombol submit
 function submitButton() {
+    yourAce = 0;
+    dealerAce = 0;
+    // Membangun kartu
+    for (let i = 0; i < cType.length; i++) {
+        for (let j = 0; j < cNum.length; j++) {
+            card.push(cNum[j] + "-" + cType[i]);
+        }
+    }
+    // Mengacak kartu
+    for (let i = 0; i < card.length; i++) {
+        let j = Math.floor(Math.random() * card.length);
+        let shuf = card[i];
+        card[i] = card[j];
+        card[j] = shuf;
+    }
     let input_bet = document.getElementById("input-bet");
     if ((input_bet.value * 1) <= 0) {
         alert("You can only input number '> 0'");
         canPlay = false;
     }
-    let money = document.getElementById("money");
+    var money = document.getElementById("money");
     let getMoney = parseInt(money.innerText) - input_bet.value;
     if (getMoney >= 0 && (input_bet.value * 1) > 0) {
         money.innerHTML = getMoney;
@@ -80,7 +83,16 @@ function submitButton() {
             addCard.src = "Assets/" + getYourCard + ".png";
             addCard.style.height = 150 + "px";
             yourResult += getCardValue(getYourCard);
+            if (getCardValue(getYourCard) == 11) {
+                yourAce += 1;
+            }
             document.getElementById("your-card").append(addCard);
+        }
+
+        // Menyesuaikan nilai kartu As kamu
+        while (yourResult > 21 && yourAce > 0) {
+            yourResult -= 10;
+            yourAce -= 1;
         }
         document.getElementById("your-result").innerHTML = yourResult;
 
@@ -88,9 +100,18 @@ function submitButton() {
         let addCard = document.createElement("img");
         let getDealerCard = card.pop();
         dealerResult = getCardValue(getDealerCard);
+        if (getCardValue(getDealerCard) == 11) {
+            dealerAce += 1;
+        }
+
         addCard.src = "Assets/" + getDealerCard + ".png";
         addCard.style.height = 150 + "px";
         document.getElementById("dealer-card").append(addCard);
+        // Menyesuaikan nilai kartu As dealer
+        while (dealerResult > 21 && dealerAce > 0) {
+            dealerResult -= 10;
+            dealerAce -= 1;
+        }
         document.getElementById("dealer-result").innerHTML = dealerResult;
 
         let addBackCard = document.createElement("img");
@@ -101,21 +122,7 @@ function submitButton() {
     }
 }
 
-// Mengambil nilai dari kartu
-function getCardValue(card) {
-    let data = card.split("-");
-    let temp = data[0];
-    if (isNaN(temp)) {
-        if (temp == "A") {
-            return 11;
-        }
-        return 10;
-    } else {
-        return parseInt(temp);
-    }
-}
-
-// Fungsi untuk tombol stand
+// Fungsi untuk tombol hit
 function hitButton() {
     let addCard = document.createElement("img");
     let getYourCard = card.pop();
@@ -123,6 +130,14 @@ function hitButton() {
     addCard.style.height = 150 + "px";
     yourResult += getCardValue(getYourCard);
     document.getElementById("your-card").append(addCard);
+    if (getCardValue(getYourCard) == 11) {
+        yourAce += 1;
+    }
+    // Menyesuaikan nilai kartu As kamu
+    while (yourResult > 21 && yourAce > 0) {
+        yourResult -= 10;
+        yourAce -= 1;
+    }
     document.getElementById("your-result").innerHTML = yourResult;
     if (yourResult > 21) {
         standButton();
@@ -139,40 +154,70 @@ function standButton() {
         let addCard = document.createElement("img");
         let getDealerCard = card.pop();
         dealerResult += getCardValue(getDealerCard);
+        if (getCardValue(getDealerCard) == 11) {
+            dealerAce += 1;
+        }
         addCard.src = "Assets/" + getDealerCard + ".png";
         addCard.style.height = 150 + "px";
         document.getElementById("dealer-card").append(addCard);
     }
+    // Menyesuaikan nilai kartu As dealer
+    while (dealerResult > 21 && dealerAce > 0) {
+        dealerResult -= 10;
+        dealerAce -= 1;
+    }
     document.getElementById("dealer-result").innerHTML = dealerResult;
     let result = document.getElementById("result-text");
-    if (yourResult == dealerResult) {
+    let input_bet = document.getElementById("input-bet");
+    var money = document.getElementById("money");
+    if (yourResult > 21) {
+        result.innerHTML = "You Lose!";
+    } else if (yourResult == dealerResult) {
         result.innerHTML = "Tie!";
-        let input_bet = document.getElementById("input-bet");
-        let money = document.getElementById("money");
         let getMoney = parseInt(money.innerText) + (input_bet.value * 1);
         money.innerHTML = getMoney;
-    } else if (yourResult > 21) {
-        result.innerHTML = "You Lose!";
     } else if (dealerResult > 21) {
-        document.getElementById("result-text").innerHTML = "You Win!";
-        let input_bet = document.getElementById("input-bet");
-        let money = document.getElementById("money");
-        let getMoney = parseInt(money.innerText) + (input_bet.value * 5);
-        money.innerHTML = getMoney;
+        if (yourResult == 21) {
+            result.innerHTML = "You Win, you got a BlackJack!";
+            let getMoney = parseInt(money.innerText) + (input_bet.value * 5);
+            money.innerHTML = getMoney;
+        } else {
+            result.innerHTML = "You Win!";
+            let getMoney = parseInt(money.innerText) + (input_bet.value * 2);
+            money.innerHTML = getMoney;
+        }
     } else if (yourResult > dealerResult) {
-        result.innerHTML = "You Win!";
-        let input_bet = document.getElementById("input-bet");
-        let money = document.getElementById("money");
-        let getMoney = parseInt(money.innerText) + (input_bet.value * 5);
-        money.innerHTML = getMoney;
+        if (yourResult == 21) {
+            result.innerHTML = "You Win, you got a BlackJack!";
+            let getMoney = parseInt(money.innerText) + (input_bet.value * 5);
+            money.innerHTML = getMoney;
+        } else {
+            result.innerHTML = "You Win!";
+            let getMoney = parseInt(money.innerText) + (input_bet.value * 2);
+            money.innerHTML = getMoney;
+        }
     } else if (yourResult < dealerResult) {
         result.innerHTML = "You Lose!";
     }
 
-    let money = (document.getElementById("money").innerText) * 1;
+    money = money.innerText * 1;
     if (money == 0) {
         alert("You lose, your money runs out. Press 'Reset Money' to reset your money!");
         document.getElementById("restart-button").disabled = true;
         document.getElementById("resmo-button").disabled = false;
+    }
+}
+
+// Mengambil nilai dari kartu
+function getCardValue(card) {
+    let data = card.split("-");
+    let temp = data[0];
+    if (isNaN(temp)) {
+        if (temp == "A") {
+            return 11;
+        }
+        return 10;
+    } else {
+        return parseInt(temp);
     }
 }
